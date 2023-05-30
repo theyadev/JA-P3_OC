c73ce8b07acbcf40b698af103df8861cfd108f48
Théo FASSI <tfa@subteno.com>
2023-05-30 14:14:51 +0200

    first commit

---
+++ src/main/java/com/openclassrooms/P3_OC/P3OcApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P3_OC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3OcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3OcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/P3_OC/P3OcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P3_OC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3OcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
